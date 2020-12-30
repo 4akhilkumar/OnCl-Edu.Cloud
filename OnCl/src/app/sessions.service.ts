cc62e68f42315bd0b6db00e769e4e728cca2ffc0
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2020-12-30 21:53:37 +0530

    added required CRUD operation for sessions.services.ts

--- OnCl/src/app/sessions.service.ts
+++ OnCl/src/app/sessions.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class SessionsService {
 
-  constructor() { }
+  uri = 'http://localhost:4000';
+
+  constructor(private http:HttpClient) {
+  }
+
+  getRecords(id){
+    return this.http.get(`${this.uri}/sessions/${id}`);
+  }
+
+  addRecord(student) {
+    return this.http.post(`${this.uri}/sessions/add`, student);
+  }
+
+  deleteRecord(id) {
+    return this.http.delete(`${this.uri}/sessions/delete/${id}`);
+  }
+
+  getRecordById(id){
+    return this.http.get(`${this.uri}/sessions/session/${id}`);
+  }
+
+  updateRecord(id,student){
+    return this.http.patch(`${this.uri}/sessions/edit/${id}`,student);
+  }
+
 }
