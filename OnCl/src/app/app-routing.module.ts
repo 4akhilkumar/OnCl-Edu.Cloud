9a57c8c4bb8674bb8e9077212ea0ebda186b1b94
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 13:28:16 +0530

    Imported SessionsCardViewComponent

--- OnCl/src/app/app-routing.module.ts
+++ OnCl/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { EditComponent } from './edit/edit.component';
 import { ViewComponent } from './view/view.component';
 import { ViewSessionComponent } from './view-session/view-session.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SessionsCardViewComponent } from './sessions-card-view/sessions-card-view.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path:'session',component:SessionComponent},
   {path:'home',component:HomeComponent},
   {path:'welcome',component:WelcomeComponent},
+  {path:'sessions-card-view',component:SessionsCardViewComponent,canActivate: [AuthGuard]},
   {path:'edit/:id',component:EditComponent,canActivate: [AuthGuard]},
   {path:'view/:id',component:ViewSessionComponent,canActivate: [AuthGuard]},
 ];
