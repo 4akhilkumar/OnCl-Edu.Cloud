4a98b425275a61c51bdc0a0fbaa11f81aeb0ecbe
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 17:49:27 +0530

    Update auth.guard.ts

--- OnCl/src/app/auth.guard.ts
+++ OnCl/src/app/auth.guard.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
