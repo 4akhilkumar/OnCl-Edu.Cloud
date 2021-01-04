c5821cdd3b74727f4fcf4b1dc1c21dea640de973
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 13:27:37 +0530

    Imported WelcomeComponent

--- OnCl/src/app/app-routing.module.ts
+++ OnCl/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { SessionComponent } from './session/session.component';
 import { EditComponent } from './edit/edit.component';
 import { ViewComponent } from './view/view.component';
 import { ViewSessionComponent } from './view-session/view-session.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'session',component:SessionComponent},
   {path:'home',component:HomeComponent},
+  {path:'welcome',component:WelcomeComponent},
   {path:'edit/:id',component:EditComponent,canActivate: [AuthGuard]},
   {path:'view/:id',component:ViewSessionComponent,canActivate: [AuthGuard]},
 ];
