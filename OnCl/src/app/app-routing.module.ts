52918fe9121a63e07d1c711b2e9e434d11be1ea1
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 13:27:16 +0530

    Imported ViewComponent

--- OnCl/src/app/app-routing.module.ts
+++ OnCl/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent }from './home/home.component';
 import { SessionComponent } from './session/session.component';
 import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
 import { ViewSessionComponent } from './view-session/view-session.component';
 
 const routes: Routes = [
