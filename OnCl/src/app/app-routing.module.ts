ad9702a801d0ccd8e61260cd1776bb8b7e1e0593
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 13:23:55 +0530

    Imported HomeComponent

--- OnCl/src/app/app-routing.module.ts
+++ OnCl/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent }from './home/home.component';
 import { SessionComponent } from './session/session.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'session',component:SessionComponent},
+  {path:'home',component:HomeComponent},
 ];
 
 @NgModule({
