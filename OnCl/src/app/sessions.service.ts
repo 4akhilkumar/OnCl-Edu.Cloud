0fa9063adeb7ae770df0c0c298e7661a2705e0f0
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 14:00:19 +0530

    sessions.service

--- OnCl/src/app/sessions.service.ts
+++ OnCl/src/app/sessions.service.ts
@@ -6,29 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class SessionsService {
 
-  uri = 'http://localhost:4000';
+  url = 'http://localhost:4000';
 
   constructor(private http:HttpClient) {
   }
 
   getRecords(id){
-    return this.http.get(`${this.uri}/sessions/${id}`);
+    return this.http.get(`${this.url}/sessions/${id}`);
   }
 
   addRecord(student) {
-    return this.http.post(`${this.uri}/sessions/add`, student);
+    return this.http.post(`${this.url}/sessions/add`, student);
   }
 
   deleteRecord(id) {
-    return this.http.delete(`${this.uri}/sessions/delete/${id}`);
+    return this.http.delete(`${this.url}/sessions/delete/${id}`);
   }
 
   getRecordById(id){
-    return this.http.get(`${this.uri}/sessions/session/${id}`);
+    return this.http.get(`${this.url}/sessions/session/${id}`);
   }
 
   updateRecord(id,student){
-    return this.http.patch(`${this.uri}/sessions/edit/${id}`,student);
+    return this.http.patch(`${this.url}/sessions/edit/${id}`,student);
   }
 
 }
