2c9bb3bfbdf1b1cf37f5e677e532fa0715795ff5
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-04 13:24:23 +0530

    Imported EditComponent

--- OnCl/src/app/app-routing.module.ts
+++ OnCl/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent }from './home/home.component';
 import { SessionComponent } from './session/session.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'session',component:SessionComponent},
   {path:'home',component:HomeComponent},
+  {path:'edit/:id',component:EditComponent,canActivate: [AuthGuard]},
 ];
 
 @NgModule({
