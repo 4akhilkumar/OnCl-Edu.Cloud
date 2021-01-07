699c01255f7aef56861b62906f302dc0c137dc52
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-07 15:39:34 +0530

    TimetableService

---
+++ OnCl/src/app/timetable.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimetableService {
+
+  url = 'http://localhost:4000';
+
+  constructor(private http:HttpClient) {
+  }
+
+  getRecords(id){
+    return this.http.get(`${this.url}/timetable/${id}`);
+  }
+
+  addRecord(student) {
+    return this.http.post(`${this.url}/timetable/add`, student);
+  }
+
+  deleteRecord(id) {
+    return this.http.delete(`${this.url}/timetable/delete/${id}`);
+  }
+
+  getRecordById(id){
+    return this.http.get(`${this.url}/timetable/timetable/${id}`);
+  }
+
+  updateRecord(id,student){
+    return this.http.patch(`${this.url}/timetable/edit/${id}`,student);
+  }
+
+}
